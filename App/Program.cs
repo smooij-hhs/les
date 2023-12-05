d1398d704449bf6cbfa456a9c0748497deefa98c
Steven Mooij <steven@mooij.name>
2023-12-05 09:32:05 +0100

    First import.

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace App;
+public class Program
+{
+    public static string Greet()
+    {
+        return "Hello, World!";
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Greet());
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using App;
+
+namespace Test;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal("Hello, World!", Program.Greet());
+    }
+}
