257f5a0f360006afac68ab625e1e9f92976deaed
Steven Mooij <steven@mooij.name>
2023-12-05 10:05:41 +0100

    Print secret

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -8,6 +8,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Console.WriteLine(Environment.GetEnvironmentVariable("SECRET"));
         Assert.Equal("Hello, World!", Program.Greet());
     }
 }
